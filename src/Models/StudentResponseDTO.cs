84bd5d6ec43a71fbc2ce106913a00d04d3adad2b
Davi Luquini <davi262016@gmail.com>
2025-10-06 22:57:31 -0300

    minor adjusts

--- src/Models/StudentResponseDTO.cs
+++ src/Models/StudentResponseDTO.cs
@@ -3,7 +3,7 @@
     public class StudentResponseDTO
     {
         public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public required string Name { get; set; }
         public int Grade { get; set; }
         public char UniqueLetter { get; set; }
     }
